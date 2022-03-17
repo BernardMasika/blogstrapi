cb24a5c81b123dc159de1be9b5c2cfe8eae091c2
BernardMasika <fredy.masika@gmail.com>
2022-03-18 00:27:38 +0300

    watch a 30min youtube video the one with aws

--- config/database.js
+++ config/database.js
@@ -1,11 +1,39 @@
 const path = require('path');
+const parse = require('pg-connection-string').parse;
 
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'sqlite',
+module.exports = ({ env }) => {
+
+  if (env('NODE_ENV') === 'production') {
+    const {host, port, database, password, user} = parse(process.env.DATABASE_URL)
+    return {
+      defaultConnection: "default",
+      connections: {
+        default: {
+          connector: "bookshelf",
+          settings: {
+            client: "postgres",
+            host,
+            port,
+            database,
+            username: user,
+            password,
+            ssl: { rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false) },
+          },
+          options: {
+            ssl: false,
+          },
+        },
+      },
+    }
+  }
+
+  return {
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      client: 'sqlite',
+      connection: {
+        filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      },
+      useNullAsDefault: true,
     },
-    useNullAsDefault: true,
-  },
-});
+  }
+};
