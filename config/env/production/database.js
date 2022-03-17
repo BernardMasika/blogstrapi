a381c3e26a54734105d275274c5fb86500bb4b56
BernardMasika <fredy.masika@gmail.com>
2022-03-17 20:32:55 +0300

    i hard coded the env secrets

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -10,11 +10,11 @@ module.exports = ({ env }) => {
         connector: "bookshelf",
         settings: {
           client: "postgres",
-          host,
-          port,
-          database,
-          username: user,
-          password,
+          host: env('DATABASE_HOST', 'ec2-52-214-125-106.eu-west-1.compute.amazonaws.com'),
+          port: env.int('DATABASE_PORT', 5432),
+          database: env('DATABASE_NAME', 'd4d2s9lbg72akt'),
+          username: env('DATABASE_USERNAME', 'glzbturqmtjqau'),
+          password: env('DATABASE_PASSWORD', '6f009bb3ec8bacac0c8105da9534d8be6c94615e5a4c57fdbb52d665a365cb57'),
           ssl: { rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false) },
         },
         options: {
