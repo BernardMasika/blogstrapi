60f18116ee4cfbaa459beee74973021c3b82a2de
BernardMasika <fredy.masika@gmail.com>
2022-03-18 23:52:54 +0300

    found a similiar stackover problem

--- config/database.js
+++ config/database.js
@@ -1,39 +1,80 @@
-const path = require('path');
-const parse = require('pg-connection-string').parse;
-
-module.exports = ({ env }) => {
-
-  if (env('NODE_ENV') === 'production') {
-    const {host, port, database, password, user} = parse(process.env.DATABASE_URL)
-    return {
-      defaultConnection: "default",
-      connections: {
-        default: {
-          connector: "bookshelf",
-          settings: {
-            client: "postgres",
-            host,
-            port,
-            database,
-            username: user,
-            password,
-            ssl: { rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false) },
-          },
-          options: {
-            ssl: false,
-          },
-        },
-      },
-    }
-  }
-
-  return {
+module.exports = ({ env }) => ({
+  defaultConnection: "default",
+  connection: {
+    client: "postgres",
     connection: {
-      client: 'sqlite',
-      connection: {
-        filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
-      },
-      useNullAsDefault: true,
+      host: env("DATABASE_HOST", "localhost"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "bank"),
+      username: env("DATABASE_USERNAME", "postgres"),
+      password: env("DATABASE_PASSWORD", "0000"),
+      schema: env("DATABASE_SCHEMA", "public"),
     },
   }
-};
+});
+
+
+
+
+
+
+
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: 'postgres',
+//     connection: {
+//       host: `/cloudsql/${env('INSTANCE_CONNECTION_NAME')}`,
+//       database: env('DATABASE_NAME'),
+//       user: env('DATABASE_USER'),
+//       password: env('DATABASE_PASSWORD'),
+//     },
+//   },
+// });
+
+
+
+
+
+
+
+
+
+// const path = require('path');
+// const parse = require('pg-connection-string').parse;
+//
+// module.exports = ({ env }) => {
+//
+//   if (env('NODE_ENV') === 'production') {
+//     const {host, port, database, password, user} = parse(process.env.DATABASE_URL)
+//     return {
+//       defaultConnection: "default",
+//       connections: {
+//         default: {
+//           connector: "bookshelf",
+//           settings: {
+//             client: "postgres",
+//             host,
+//             port,
+//             database,
+//             username: user,
+//             password,
+//             ssl: { rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false) },
+//           },
+//           options: {
+//             ssl: false,
+//           },
+//         },
+//       },
+//     }
+//   }
+//
+//   return {
+//     connection: {
+//       client: 'sqlite',
+//       connection: {
+//         filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+//       },
+//       useNullAsDefault: true,
+//     },
+//   }
+// };
--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,26 +1,65 @@
-const { parse } = require("pg-connection-string");
-
-module.exports = ({ env }) => {
-  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
-
-  return {
-    defaultConnection: "default",
-    connections: {
-      default: {
-        connector: "bookshelf",
-        settings: {
-          client: "postgres",
-          host,
-          port,
-          database,
-          username: user,
-          password,
-          ssl: { rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false) },
-        },
-        options: {
-          ssl: env.bool('DATABASE_SSL', false),
-        },
+const parse = require('pg-connection-string').parse;
+const config = parse(process.env.DATABASE_URL);
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'postgres',
+    connection: {
+      host: config.host,
+      port: config.port,
+      database: config.database,
+      user: config.user,
+      password: config.password,
+      ssl: {
+        rejectUnauthorized: false
       },
     },
-  };
-};
+    debug: false,
+  },
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const { parse } = require("pg-connection-string");
+//
+// module.exports = ({ env }) => {
+//   const { host, port, database, user, password } = parse(env("DATABASE_URL"));
+//
+//   return {
+//     defaultConnection: "default",
+//     connections: {
+//       default: {
+//         connector: "bookshelf",
+//         settings: {
+//           client: "postgres",
+//           host,
+//           port,
+//           database,
+//           username: user,
+//           password,
+//           ssl: { rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false) },
+//         },
+//         options: {
+//           ssl: env.bool('DATABASE_SSL', false),
+//         },
+//       },
+//     },
+//   };
+// };
