8f49809ee90b48bc51228f622f90ae8eb0ffcfb0
BernardMasika <fredy.masika@gmail.com>
2022-03-19 03:05:14 +0300

    trying using the default settings

--- config/database.js
+++ config/database.js
@@ -1,41 +1,37 @@
-module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'bookshelf',
-      settings: {
-        client: 'postgres',
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'bernard'),
-        username: env('DATABASE_USERNAME', 'bernard'),
-        password: env('DATABASE_PASSWORD', 'rawl'),
-      },
-      options: {
-        ssl: false,
-      },
-    },
-  },
-});
-
-
-
-
-
-
-
-
 // module.exports = ({ env }) => ({
-//   connection: {
-//     client: 'postgres',
-//     connection: {
-//       host: `/cloudsql/${env('INSTANCE_CONNECTION_NAME')}`,
-//       database: env('DATABASE_NAME'),
-//       user: env('DATABASE_USER'),
-//       password: env('DATABASE_PASSWORD'),
+//   defaultConnection: 'default',
+//   connections: {
+//     default: {
+//       connector: 'bookshelf',
+//       settings: {
+//         client: 'postgres',
+//         host: env('DATABASE_HOST', '127.0.0.1'),
+//         port: env.int('DATABASE_PORT', 5432),
+//         database: env('DATABASE_NAME', 'bernard'),
+//         username: env('DATABASE_USERNAME', 'bernard'),
+//         password: env('DATABASE_PASSWORD', 'rawl'),
+//       },
+//       options: {
+//         ssl: false,
+//       },
 //     },
 //   },
 // });
+
+
+
+
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'postgres',
+    connection: {
+      host: `/cloudsql/${env('INSTANCE_CONNECTION_NAME')}`,
+      database: env('DATABASE_NAME'),
+      user: env('DATABASE_USER'),
+      password: env('DATABASE_PASSWORD'),
+    },
+  },
+});
 
 
 
